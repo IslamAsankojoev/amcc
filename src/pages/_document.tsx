87f091fe7819d0cb27deb72134757d72ff1ba425
Islam Asankojoev <islam.asankojoev@gmail.com>
2023-11-30 17:15:47 +0600

    fix smooth

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={
+      {
+        scrollBehavior: 'smooth',
+      }
+    }>
       <Head />
       <body className={`${montserrat.className}`}>
         <Main />
